5c3af8f9a621dbb7a92bd507876eb9283505e86c
SergioCainzo <sergiocainzo@gmail.com>
2024-11-20 11:38:32 -0300

    Estilizando componentes

--- src/Formulario.js
+++ src/Formulario.js
@@ -1,16 +1,17 @@
-function Formulario (){
-    return(
-        <form>
-            <input type="text" placeholder="Nome" />
-            <input type="text" placeholder="Marca" />
+function Formulario() {
+  return (
+    <form>
+      <input type="text" placeholder="Nome" className="form-control" />
+      <input type="text" placeholder="Marca" className="form-control" />
 
 
-            <input type="button" value="Cadastro" />
-            <input type="button" value="Alterar" />
-            <input type="button" value="Remover" />
-            <input type="button" value="Cancelar" />
-        </form>
-    )
+
+      <input type="button" value="Cadastro" className="btn btn-success" />
+      <input type="button" value="Alterar" className="btn btn-warning" />
+      <input type="button" value="Remover" className="btn btn-danger" />
+      <input type="button" value="Cancelar" className="btn btn-secondary" />
+    </form>
+  );
 }
 
 export default Formulario;
