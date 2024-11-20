6829064f79bdfb17500229ad59238971a42867de
SergioCainzo <sergiocainzo@gmail.com>
2024-11-20 11:25:04 -0300

    Estrutura do formulario

--- src/Formulario.js
+++ src/Formulario.js
@@ -1,6 +1,15 @@
 function Formulario (){
     return(
-        <h1>Formulario</h1>
+        <form>
+            <input type="text" placeholder="Nome" />
+            <input type="text" placeholder="Marca" />
+
+
+            <input type="button" placeholder="Cadastro" />
+            <input type="button" placeholder="Alterar" />
+            <input type="button" placeholder="Remover" />
+            <input type="button" placeholder="Cancelar" />
+        </form>
     )
 }
 
