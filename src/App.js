0d2279d52eb3c32ecebecafb35a98554639f0c81
SergioCainzo <sergiocainzo@gmail.com>
2024-11-20 11:55:30 -0300

    Obtendo os produtos

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Formulario from "./Formulario";
 import Tabela from "./Tabela";
 
 function App() {
-
+  //UseState
   const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState();
 
+  //UseEffect
+  useEffect(() => {
+    fetch("http://localhost:8080/listar")
+      .then((retorno) => retorno.json())
+      .then((retorno_convertido) => setProdutos(retorno_convertido));
+  }, []);
 
-
-
-
-
+  //Retorno
   return (
     <div>
+      <p>{JSON.stringify(produtos)}</p>
       <h1>Formulário de Cadastro</h1>
-      <Formulario 
-      botao={btnCadastrar}/>
+      <Formulario botao={btnCadastrar} />
 
       <h1>Tabela de Produtos</h1>
       <Tabela />
